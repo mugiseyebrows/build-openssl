2b796d1efaba56e0e246dd81783bff2bf6ec86ec
Doronin Stanislav <mugisbrows@gmail.com>
2025-07-29 17:26:23 +0300

    nasm

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 rem This file is generated from build.pbat, all edits will be lost
-set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;C:\Windows\System32;C:\Program Files\7-Zip;%PATH%
+set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;C:\Windows\System32;C:\Program Files\7-Zip;C:\Program Files\NASM;%PATH%
 call vcvars64.bat
 curl -L -o openssl-3.5.1.tar.gz https://github.com/openssl/openssl/releases/download/openssl-3.5.1/openssl-3.5.1.tar.gz
 7z x -y openssl-3.5.1.tar.gz
 7z x -y openssl-3.5.1.tar
-dir
+choco install nasm
 cd openssl-3.5.1
 perl Configure --prefix=C:/openssl-3.5.1
 nmake
