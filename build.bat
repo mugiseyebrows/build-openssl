681475ef48b833a204ba79b17735d70305d1aa6b
Doronin Stanislav <mugisbrows@gmail.com>
2025-07-29 17:19:40 +0300

    test

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ call vcvars64.bat
 curl -L -o openssl-3.5.1.tar.gz https://github.com/openssl/openssl/releases/download/openssl-3.5.1/openssl-3.5.1.tar.gz
 7z x -y openssl-3.5.1.tar.gz
 7z x -y openssl-3.5.1.tar
+dir
 cd openssl-3.5.1
 perl Configure --prefix=C:/openssl-3.5.1
 nmake
